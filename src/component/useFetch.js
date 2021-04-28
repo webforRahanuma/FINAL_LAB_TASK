37f1150d10fde8d9033cb8f184ed9aa66bb684d1
webforRahanuma <rahanumatasnim98@gmail.com>
2021-04-28 16:08:12 +0600

    Online Diary

--- src/component/useFetch.js
+++ src/component/useFetch.js
@@ -1,7 +1,26 @@
-import {useEffect} from 'react'
+import {useState, useEffect} from 'react';
 
-const useFetch = () => {
+
+export const useFetch = (url)=>{
+
+    const [status, setStatus] = useState(true);
+    const [users, setUsers] = useState([]);
+
+    const getUserlist = async ()=>{
+        // const response = await fetch(url, {
+        // 	mode:'no-cors'
+        // });
+        const response = await fetch(url);
+        const data = await response.json();
+        
+        console.log(data);
+        setUsers(data);
+        setStatus(false);
+    }
+
+    useEffect(()=>{
+    	getUserlist();
+    }, []);
+    
 
 }
-
-export default useFetch
